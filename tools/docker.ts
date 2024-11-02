a657a619c900e1ec9a0405afe1bc9de1ff43f8c0
andrei <b1naryth1ef@gmail.com>
2024-11-02 23:38:17 +0000

    tools/docker: hydr0 docker registry & cli args

--- tools/docker.ts
+++ tools/docker.ts
@@ -1,6 +1,14 @@
 #!/usr/bin/env -S deno run --allow-all --node-modules-dir=auto
 
 import { exec } from "./lib.ts";
+import { parseArgs } from "jsr:@std/cli/parse-args";
+
+const flags = parseArgs(Deno.args, {
+  boolean: ["push"],
+  string: ["tag"],
+});
+
+const tag = flags.tag || "latest"
 
 console.log("Generating bootstrap deb...");
 const cwd = Deno.cwd();
@@ -9,4 +17,9 @@ await exec("nfpm pkg --packager deb");
 Deno.chdir(cwd);
 
 console.log("Generating docker image...");
-await exec("docker build -t github.com/b1naryth1ef/pkg:latest .");
+await exec(`docker build -t docker.hydr0.com/pkg/pkg:${tag} .`, { inherit: true });
+
+if (flags.push) {
+  console.log("Pushing docker image...");
+  await exec(`docker push docker.hydr0.com/pkg/pkg:${tag}`, { inherit: true });
+}
